0c73e49bae050696a6052091b659327d1b25bda6
soroush <soroushsoleimani1s@gmail.com>
2026-02-11 13:22:52 +0330

    Configure baseline CNN for 8 emotion classes

---
+++ src/models/baseline_model.py
@@ -0,0 +1,28 @@
+from tensorflow.keras import layers, models
+
+def build_model(input_shape=(48, 48, 1), num_classes=8):
+    model = models.Sequential()
+
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=input_shape))
+    model.add(layers.BatchNormalization())
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Dropout(0.2))
+
+    model.add(layers.Conv2D(64, (3, 3), activation='relu', padding='same'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Dropout(0.2))
+
+    model.add(layers.Conv2D(128, (3, 3), activation='relu', padding='same'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Dropout(0.2))
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(128, activation='relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(num_classes, activation='softmax'))
+
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
